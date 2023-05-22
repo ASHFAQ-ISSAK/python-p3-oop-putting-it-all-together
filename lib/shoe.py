fb6d5312076ad4a6b0ab22be9f2e4067e93140c1
ASHFAQ ISSAK <ashfaq.abdi@gmail.com>
2023-05-22 10:43:30 +0300

    finishd book and shoe

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
+
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "Used"
+
+    def is_size_an_integer(self):
+        if not isinstance(self.size, int):
+            print("size must be an integer")
+        else:
+            print(self.size)
+
+    def cobble(self):
+        self.condition = "New"
+        if self.condition != "New":
+            print("Your shoe is as good as new!")
