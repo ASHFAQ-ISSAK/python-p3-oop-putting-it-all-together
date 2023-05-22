b4b5dd2a5fdca89f01790cbdca27c8f69a827014
ASHFAQ ISSAK <ashfaq.abdi@gmail.com>
2023-05-22 10:28:41 +0300

    finishd book

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
+
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    def is_page_count_integer(self):
+        if not isinstance(self.page_count, int):
+            print("page_count must be an integer")
+        else:
+            print(self.page_count)
